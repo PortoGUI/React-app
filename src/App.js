2a8725ea5f2d9a5455190a4d09edefa607eb2d8d
PortoGUI <guibusiness.2000@outlook.com>
2024-04-03 20:04:39 -0300

    Tratando Eventos

--- src/App.js
+++ src/App.js
@@ -18,21 +18,36 @@ class App extends Component {
                 data: String(new Date()),
                 texto: 'this is a default message too'
             },
-        ]
+        ],
+        novoComentario: {
+            nome: '',
+            email: '',
+            texto: ''
+        }
     }
 
-    addComment = () => {
-        const newComment = {
-            nome: 'Brian',
-            email: 'Brian@email.com',
-            data: String(new Date()),
-            texto: 'this is a inserted message'
-        }
+    addComment = event => {
+        event.preventDefault()
+        // const newComment = {
+        //     nome: 'Brian',
+        //     email: 'Brian@email.com',
+        //     data: String(new Date()),
+        //     texto: 'this is a inserted message'
+        // }
 
         /*let lista = this.state.comentarios
         lista.push(newComment)
         this.setState({comentarios: lista})*/
-        this.setState({comentarios: [...this.state.comentarios, newComment]})
+
+        const newComment = {...this.state.novoComentario, data: String(new Date())}
+        this.setState({
+            comentarios: [...this.state.comentarios, newComment],
+            novoComentario: {nome: '', email: '', texto: ''}
+        })
+    }
+
+    changeByName = event => {
+        this.setState({novoComentario: {...this.state.novoComentario, [event.target.name]: event.target.value}})
     }
 
     render() {
@@ -46,7 +61,23 @@ class App extends Component {
                             </Comentario>
                     ))}
 
-                    <button onClick={this.addComment}>Adicionar Comentario</button>
+                    <form method="post" onSubmit={this.addComment}>
+                        <h2>Adicionar Comentario</h2>
+                        <div>
+                            <input value={this.state.novoComentario.nome} onChange={this.changeByName} type="text"
+                                   name="nome" placeholder="Digite um nome"/>
+                        </div>
+                        <div>
+                            <input value={this.state.novoComentario.email} onChange={this.changeByName} type="email"
+                                   name="email" placeholder="digite um e-mail"/>
+                        </div>
+                        <div>
+                            <textarea value={this.state.novoComentario.texto} onChange={this.changeByName}
+                                      name="texto" rows="4"/>
+                        </div>
+                        <button>Adicionar</button>
+                    </form>
+
                 </div>
         );
     }
