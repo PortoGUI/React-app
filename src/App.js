eaf677658348001a812e9fcb1dd8b4aeb218f8bd
PortoGUI <guibusiness.2000@outlook.com>
2024-04-03 18:59:23 -0300

    Trabalhando com Estado dos componentes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
+import {Component} from "react";
 import './App.css';
 import Comentario from "./components/comentario/Comentario";
 
-function App() {
-    return (
-            <div className="App">
-                <h1>React Project</h1>
-                <Comentario name="Mario" email="texte@email.com" date={new Date()}>
-                    this is a default message
-                </Comentario>
-                <Comentario name="Andrew" email="texte@email.com" date={new Date()}>
-                    this is a default message
-                </Comentario>
-                <Comentario name="Chris" email="texte@email.com" date={new Date()}>
-                    this is a default message
-                </Comentario>
-            </div>
-    );
+class App extends Component {
+
+    state = {
+        comentarios: [
+            {
+                nome: 'Jhon',
+                email: 'Jhon@email.com',
+                data: String(new Date()),
+                texto: 'this is a default message'
+            },
+            {
+                nome: 'Hanna',
+                email: 'Hanna@email.com',
+                data: String(new Date()),
+                texto: 'this is a default message too'
+            },
+        ]
+    }
+
+    render() {
+        return (
+                <div className="App">
+                    <h1>React Project</h1>
+                    {
+                        this.state.comentarios.map((comentario, i) => (
+                                <Comentario key={i} name={comentario.nome} email={comentario.email}
+                                            date={comentario.data}>
+                                    {comentario.texto}
+                                </Comentario>
+                        ))
+                    }
+                </div>
+        );
+    }
 }
 
 export default App;
--- src/components/comentario/Comentario.js
+++ src/components/comentario/Comentario.js
@@ -1,13 +1,15 @@
 import React from "react";
 import './comentario.css'
 
-const Comentario = props => (
-        <div className="comentario">
-            <h2><b>{props.name}</b></h2>
-            <p>{props.email}</p>
-            <p>{props.children}</p>
-            <p>{String(props.date)}</p>
-        </div>
-)
+const Comentario = props => {
+    return (
+            <div className="comentario">
+                <h2><b>{props.name}</b></h2>
+                <p>{props.email}</p>
+                <p>{props.children}</p>
+                <p>{props.date}</p>
+            </div>
+    )
+}
 
 export default Comentario;
