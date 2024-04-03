99f220dc8e81e52bc4b19380cbfb0dfa818aee98
PortoGUI <guibusiness.2000@outlook.com>
2024-04-03 18:19:26 -0300

    Construindo componentes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Comentario from "./components/comentario/Comentario";
+
+function App() {
+    return (
+            <div className="App">
+                <h1>React Project</h1>
+                <Comentario></Comentario>
+                <Comentario></Comentario>
+                <Comentario></Comentario>
+            </div>
+    );
+}
+
+export default App;
---
+++ src/components/comentario/Comentario.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './comentario.css'
+
+const Comentario = () => (
+        <div className="comentario">
+            <div>Hello World</div>
+            <div>How are you?</div>
+        </div>
+)
+
+export default Comentario;
